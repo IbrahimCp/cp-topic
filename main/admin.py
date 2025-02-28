5332a1543497cebc9591ac955714c85f70dc6f97
ibrahimCp <ibragim27072005@icloud.com>
2025-02-28 10:46:35 +0500

    -

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from main.models import(Topic, Resource, Problem, CodeSnippet)
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Resource)
+admin.site.register(Problem)
+admin.site.register(CodeSnippet)
